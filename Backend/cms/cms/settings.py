6ff8a32b4a0ccef491463d4f1b459025f246ade4
Pramod-0215 <pramodray0215@gmail.com>
2020-01-30 18:43:18 +0530

    add postgreSql database

--- Backend/cms/cms/settings.py
+++ Backend/cms/cms/settings.py
@@ -80,9 +80,13 @@ WSGI_APPLICATION = 'cms.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+     'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'cms',
+        'USER': 'cmsuser',
+        'PASSWORD': 'pa$$w0rd',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
